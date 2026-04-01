4e6691e5a00f9cfa31b673f7a9b967357027864d
K Shams Nahid <102136391+shamsnahidk@users.noreply.github.com>
2026-03-31 19:14:11 -0500

    Update transformations.py

--- src/transformations.py
+++ src/transformations.py
@@ -5,21 +5,26 @@ import numpy as np
 def translate(image, tx, ty):
     h, w = image.shape[:2]
     matrix = np.float32([[1, 0, tx], [0, 1, ty]])
-    return cv2.warpAffine(image, matrix, (w, h))
+    transformed = cv2.warpAffine(image, matrix, (w, h))
+    return transformed, matrix
 
 
 def rotate(image, angle):
     h, w = image.shape[:2]
     center = (w // 2, h // 2)
     matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
-    return cv2.warpAffine(image, matrix, (w, h))
+    transformed = cv2.warpAffine(image, matrix, (w, h))
+    return transformed, matrix
 
 
 def scale(image, fx, fy):
-    return cv2.resize(image, None, fx=fx, fy=fy)
+    matrix = np.float32([[fx, 0, 0], [0, fy, 0]])
+    transformed = cv2.resize(image, None, fx=fx, fy=fy)
+    return transformed, matrix
 
 
 def shear(image, shx=0.0, shy=0.0):
     h, w = image.shape[:2]
     matrix = np.float32([[1, shx, 0], [shy, 1, 0]])
-    return cv2.warpAffine(image, matrix, (w, h))
+    transformed = cv2.warpAffine(image, matrix, (w, h))
+    return transformed, matrix
